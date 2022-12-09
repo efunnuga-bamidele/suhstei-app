8894f82fd416da18c2bc44150a777ab780581fe7
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-09 18:25:10 +0100

    fix:Page Naming

--- src/pages/login/login.page.js
+++ src/pages/login/login.page.js
@@ -18,7 +18,9 @@ export default function LoginPage() {
                 </div>
                 <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
                 <form>
-                
+                <div className="relative z-0 mb-6 w-full group">
+                        <h1 className='text-2xl sm:text-3xl md:text-5xl font-bold my-auto mb-4 text-primary text-right'>Sign In</h1>
+                    </div>
                 <div className="relative z-0 mb-6 w-full group">
                         <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                         <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
