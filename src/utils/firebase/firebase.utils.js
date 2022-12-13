41bba1159ce71df7fc5b823dfd1962accfad2808
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-13 16:00:19 +0100

    feat:Create firebase functions for email and google signup

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
-  
-} from 'firebase/auth';
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  FacebookAuthProvider
 
+} from 'firebase/auth';
+import  {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCQn8z4Z8OnN3xXCHRafwvgqhI_MIeWbag",
@@ -17,4 +33,62 @@ const firebaseConfig = {
   //https://suhstei-d0d28.firebaseapp.com/__/auth/handler
 
 initializeApp(firebaseConfig);
-  const db = ""
+
+// Google authentication
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+
+// Firestore initialization
+export const db = getFirestore();
+
+// Function to create new user on authentication
+export const createUserDocumentFromAuth = async (userAuth, additionalinformation) => {
+
+  if (!userAuth) return;
+
+      const userDocRef = doc(db, 'users', userAuth.uid);
+      const userSnapshot = await getDoc(userDocRef);
+
+      if (!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+          await setDoc(userDocRef, {
+              displayName,
+              email,
+              createdAt,
+              ...additionalinformation
+          });
+        }catch (error){
+          console.log('error creating the user : ', error.message)
+        }
+      }
+      return userDocRef;
+}
+
+//create new user with email and password
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+//signin with email and password
+export const sighAuthUserInWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+};
+
+//Signout user
+export const signOutUser = () => signOut(auth);
+
+//Observer to monitor state change in authentication
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
