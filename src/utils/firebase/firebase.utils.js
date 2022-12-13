6e74a3b744e2009aaf9a7e4fbff35aa77d7c1ced
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-13 15:25:51 +0100

    feat:Initialize firebase, create folder structure and import dependencies

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  
+} from 'firebase/auth';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQn8z4Z8OnN3xXCHRafwvgqhI_MIeWbag",
+    authDomain: "suhstei-d0d28.firebaseapp.com",
+    projectId: "suhstei-d0d28",
+    storageBucket: "suhstei-d0d28.appspot.com",
+    messagingSenderId: "407226505523",
+    appId: "1:407226505523:web:2972a1824fe5a9de9e0646"
+  };
+
+  //https://suhstei-d0d28.firebaseapp.com/__/auth/handler
+
+initializeApp(firebaseConfig);
+  const db = ""
