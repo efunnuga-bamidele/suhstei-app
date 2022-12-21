dee9610989cc3e3f3eb56be98d8ad8b6051f2639
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-21 12:03:55 +0100

    feat:Create Sidebar Panel

--- src/pages/dashboard/dashboard.page.js
+++ src/pages/dashboard/dashboard.page.js
@@ -1,14 +1,20 @@
 import Navigation from "../../components/navigation/navigation.component";
 import Footer from "../../components/footer/footer.component";
+import SidebarNavigation from "../../components/sidebar/sidebar.component";
 
 
 export default function DashboardPage(){
     return (
-        <div>
+        <div className='bg-gray-100 mx-1 font-body scroll-smooth'>
             <Navigation />
-                <h1 className='mt-20'>
-                    Dashboard page
-                </h1>
+                <main className="bg-gray-300 mt-5 flex flex-wrap-reverse md:flex-nowrap">
+                  
+                    <SidebarNavigation />
+                    <section className="bg-white mt-12 mb-2 p-2 w-full mr-1">
+                        <h1 className="font-bold text-lg text-center underline">My Dashboard</h1>
+                        <p> Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.Get some of the most exciting books you have always been looking for.</p>
+                    </section>
+                </main>
             <Footer />
         </div>
     )
