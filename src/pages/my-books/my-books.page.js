b46648ff3bf376d2fffdaad3cf96a31c4363988e
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-04 10:16:25 +0100

    fix:Corrective Sidenav responsivness

--- src/pages/my-books/my-books.page.js
+++ src/pages/my-books/my-books.page.js
@@ -41,24 +41,19 @@ export default function MyBooksPage(){
     // console.log(myBooksMap)
 
     return(
-        <div className='bg-gray-100 mx-1 font-body scroll-smooth h-0'>
+        <div className='bg-gray-100 font-body scroll-smooth h-0'>
         <Navigation />
             <main className="bg-gray-300 mt-5 flex flex-wrap-reverse md:flex-nowrap">
-                    <SidebarNavigation />
-                    <section className="bg-white mt-12 mb-2 p-2 w-full mr-1">
+                    <SidebarNavigation/>
+                    <section className="bg-white mt-12 m-2 p-2 w-full">
                     {/* <section className='bg-gray-100 mt-20'> */}
                         <h1 className="font-bold text-lg text-center underline">My Books</h1>
-                        <div className='m-6 overflow-x-hidden grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 px-2'>{/* */}
+                        <div className='overflow-x-hidden grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 px-2'>{/* */}
                             {isLoading && <p>Loading.........</p>}
                             {myBooks && myBooks.map((item, index) => (
-                                // <p key={index}> {item.book_author} </p>
                                 <BookItem key={index} bookImage = {item.imageUrl} title ={item.book_title} author ={item.book_author} owner={item.book_owner} />
                             ))}
                         </div>
-
-                        
-
-                        
                     </section>
             </main>
         <Footer />
