6fe7606b252eca3d5c2d25a149b9667aeecf4017
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-01-24 08:30:15 +0100

    feat:Create Redux Reducer, Action, Type & Selector for BookRequest

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { myBooksReducer } from "./myBooks/myBooks.reducer";
 import { userBookReducer } from "./userBook/userBook.reducer";
 import { useReducer } from './user/user.reducer';
+import { bookRequestReducer } from "./booksRequest/booksRequest.reducer";
 
 export const rootReducer = combineReducers({
     user: useReducer,
     mybooks: myBooksReducer,
-    userbook: userBookReducer
+    userbook: userBookReducer,
+    bookrequest: bookRequestReducer
 })
