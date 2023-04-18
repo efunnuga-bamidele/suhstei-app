f5724bc74d908380bf2ddb69b26e346f73e4039e
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-04-18 08:39:27 +0100

    feat:About Page Created

--- src/pages/about-us/about-us.page.js
+++ src/pages/about-us/about-us.page.js
@@ -4,12 +4,22 @@ import Navigation from '../../components/navigation/navigation.component'
 
 export default function AboutUsPage() {
   return (
-    <div className='h-0'>
-        <Navigation />
-        <h1 className='mt-20'>
-            About us.page
-        </h1>
-        <Footer />
+    <div className='bg-gray-100 mx-1 font-body scroll-smooth h-0'>
+      <Navigation />
+      <main className=' flex justify-center items-center bg-gray-100 mt-20'>{/*who we are*/}
+        <section>
+          <div className='black justify-center mt-6'>
+            <h5 className="block text-3xl font-normal tracking-tight text-slate-600 dark:text-white text-center">
+              Who We Are
+            </h5>
+            <p className="block text-lg font-thin tracking-tight text-slate-800 dark:text-white text-center mx-5 md:mx-40 my-10">
+              SUHSTEI is a sustainable social media platform, where books are shared between readers and book lovers in your community.
+              We are currently pilot testing and soon we will be out there to make the world green and clean, one book at a time.
+            </p>
+          </div>
+        </section>{/*who we are*/}
+      </main>
+      <Footer />
     </div>
   )
 }
